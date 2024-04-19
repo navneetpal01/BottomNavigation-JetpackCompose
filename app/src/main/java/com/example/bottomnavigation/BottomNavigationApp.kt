b4e1ebdebb9bc870e6bb9ca20094bfbb6eee345d
navneet <navneetpalgithub@gmail.com>
2024-04-19 23:02:22 +0530

    19

--- app/src/main/java/com/example/bottomnavigation/BottomNavigationApp.kt
+++ app/src/main/java/com/example/bottomnavigation/BottomNavigationApp.kt
@@ -1,14 +1,35 @@
 package com.example.bottomnavigation
 
+import androidx.compose.foundation.layout.WindowInsets
+import androidx.compose.foundation.layout.add
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.navigationBars
+import androidx.compose.foundation.layout.navigationBarsPadding
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.windowInsetsBottomHeight
+import androidx.compose.material3.Icon
+import androidx.compose.material3.LocalContentColor
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.NavigationBar
+import androidx.compose.material3.NavigationBarItem
+import androidx.compose.material3.NavigationBarItemColors
+import androidx.compose.material3.NavigationBarItemDefaults
 import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
 import androidx.compose.runtime.remember
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.unit.dp
+import androidx.navigation.NavHostController
+import androidx.navigation.compose.currentBackStackEntryAsState
 import androidx.navigation.compose.rememberNavController
 import com.example.bottomnavigation.cources.CourseTabs
 import com.example.bottomnavigation.nvgraph.NavGraph
+import java.util.Locale
 
 
 @Composable
@@ -22,7 +43,9 @@ fun BottomNavigationApp(
 
     Scaffold(
         containerColor = Color.Blue,
-        bottomBar = {}
+        bottomBar = {
+            AppBottomBar(navController = navController, tabs = tabs)
+        }
     ) { padding ->
         NavGraph(
             finishActivity = finishActivity,
@@ -30,4 +53,72 @@ fun BottomNavigationApp(
             modifier = Modifier.padding(padding)
         )
     }
+}
+
+
+@Composable
+fun AppBottomBar(navController : NavHostController,tabs : Array<CourseTabs>){
+    val navBackStackEntry by navController.currentBackStackEntryAsState()
+
+    val currentRoute = navBackStackEntry?.destination?.route ?: CourseTabs.FEATURED.route
+
+
+    val routes = remember { CourseTabs.values().map { it.route }}
+
+    if (currentRoute in routes){
+        NavigationBar(
+            modifier = Modifier.windowInsetsBottomHeight(
+                WindowInsets.navigationBars.add(WindowInsets(bottom = 56.dp))
+            )
+        ){
+            tabs.forEach {tab ->
+                NavigationBarItem(
+                    modifier = Modifier.navigationBarsPadding(),
+                    selected = currentRoute == tab.route,
+                    onClick = {
+                        if (tab.route != currentRoute) {
+                            navController.navigate(tab.route) {
+                                popUpTo(navController.graph.startDestinationId) {
+                                    saveState = true
+                                }
+                                launchSingleTop = true
+                                restoreState = true
+                            }
+                        }
+                    },
+                    icon = {
+                        Icon(painter = painterResource(id = tab.icon), contentDescription = "null")
+                    },
+                    label = {
+                        Text(text = stringResource(id = tab.title).uppercase(locale = Locale.getDefault()))
+                    },
+                    alwaysShowLabel = false,
+                    colors = NavigationBarItemDefaults.colors(
+                        selectedIconColor = MaterialTheme.colorScheme.secondary,
+                        unselectedIconColor = LocalContentColor.current
+                    )
+                )
+            }
+        }
+
+
+
+
+    }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 }
