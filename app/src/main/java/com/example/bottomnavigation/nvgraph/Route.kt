97984acde3ae5634f879c1d2200432d22151d72f
navneet <navneetpalgithub@gmail.com>
2024-04-29 09:59:58 +0530

    19

--- app/src/main/java/com/example/bottomnavigation/nvgraph/Route.kt
+++ app/src/main/java/com/example/bottomnavigation/nvgraph/Route.kt
@@ -3,7 +3,7 @@ package com.example.bottomnavigation.nvgraph
 sealed class Route(
     val screenName : String
 ){
-    object OnBoarding : Route(screenName = "OnBoardingScreen")
+    object OnBoarding: Route(screenName = "OnBoardingScreen")
     object Courses : Route(screenName = "HomeScreen")
     object Details : Route(screenName = "DetailsScreen")
 }
